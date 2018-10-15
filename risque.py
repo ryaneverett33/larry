3794cb53baecc75ccdebeca39e9051dac4c981ea
Ryan Everett <everettr@ITIS-Students.ITNS.Purdue.edu>
2018-10-15 11:59:24 -0400

    Initial risque communication WIP

---
+++ risque.py
@@ -0,0 +1,13 @@
+import requests, lxml.html
+class Risque:
+    user = None
+    password = None
+    session = None
+
+    def __init__(self, user, password):
+        self.user = user
+        self.password = password
+        self.session = requests.session()
+
+    def getTicketData(self, ticketNumber):
+        
