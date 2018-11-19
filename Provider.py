dfa37b52bdc00a25c884802c48474da7fb2ede34
Ryan Everett <ryaneverett33@outlook.com>
2018-11-19 12:01:52 -0500

    Implemented Provider class

--- Provider.py
+++ Provider.py
@@ -5,7 +5,8 @@ class Provider:
     switch = None       # e.g. 3
     port = None         # e.g 18
     switchType = None   # e.g. c3750ep
-    intType = None      # e.g. Gi, Te, Tw
+    intType = None      # Gi, Te, Tw, Fa
+    uplink = False      # e.g. Te1/1/3
     def __init__(self, risqueString=None, switchString=None):
         if risqueString is not None:
             self.__parseProviderFromRisque(risqueString)
@@ -35,8 +36,43 @@ class Provider:
         self.port = int(providerSplit[len(providerSplit) - 1])
         self.intType = providerSplit[0][0:len(providerSplit[0] - 1)]
 
+    # Fills out the provider object from the switch type
+    # GigabitEthernet1/0/9 resolves to switch 1, port 9, intType: Gi
+    # Gi3/0/12 resolves to switch 3, port 12, intType: Gi
+    # Te1/1/3 resolves to switch 1, port 3, intType Te, uplink = True
+    # Te2/3/16 doesn't resolve as it refers to a chassis
+    # FastEthernet0/1 resolves to switch 0, port 1, intType Fa
     def __parseProviderFromSwitch(self, switch):
         if switch is None:
             raise AttributeError("switchString attribute is None")
         if type(switch) is not str:
             raise ValueError("switchString attribute is not a string")
+        # ['GigabitEthernet1', '0', '9'], ['Fa0', '8']
+        providerSplit = switch.split('/')
+        if len(providerSplit) < 2:
+            raise AttributeError("switchString is an invalid provider")
+        rawType = providerSplit[0:len(providerSplit)-1]
+        self.switch = providerSplit[len(providerSplit) - 1]
+        if len(rawType) > 2:
+            if rawType == "GigabitEthernet":
+                self.intType = "Gi"
+            elif rawType == "TwoGigabitEthernet":
+                self.intType = "Tw"
+            elif rawType == "TenGigabitEthernet":
+                self.intType = "Te"
+            elif rawType == "FastEthernet":
+                self.intType = 'Fa'
+            else:
+                raise AttributeError("switchSting is not a valid provider!")
+        else:
+            self.intType = rawType
+        # ['Fa0', '8']
+        if len(providerSplit) == 2:
+            self.port = providerSplit[1]
+        else:
+            self.port = providerSplit[2]
+            if providerSplit[1] != '0':
+                if providerSplit[1] != '1':
+                    raise AttributeError("switchString is a line card, not supported!")
+                self.uplink = True
+
