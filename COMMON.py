8a04c98c8208829caa3ba6e837945008d54a764e
BuildTools <unconfigured@null.spigotmc.org>
2019-01-16 22:20:11 -0500

    Implemented Packager

--- COMMON.py
+++ COMMON.py
@@ -1,17 +1,24 @@
 import re
+import os
 
 class Common:
     baseDir = "/home/ONEPURDUE/everettr/larry-release/"
     versionFile = "latest.version"
     releaseFile = "latest.release"
     releasesDir = "releases/"
+    ignoreDirectories = [".git", ".idea", "risque-out"]
 
     @staticmethod
     def getLatestVersion():
-        f = open(Common.baseDir + Common.versionFile)
-        version = f.readline()
-        f.close()
-        return version
+        try:
+            f = open(Common.baseDir + Common.versionFile, "r")
+            version = f.readline()
+            f.close()
+            if len(version) == 0:
+                return "0"
+            return version
+        except:
+            return "0"
 
     # -1 if version2 > version1, 0 if equal, 1 if version1 > version 2
     @staticmethod
@@ -20,3 +27,8 @@ class Common:
             return [int(x) for x in re.sub(r'(\.0+)*$', '', v).split(".")]
 
         return cmp(normalize(version1), normalize(version2))
+
+    @staticmethod
+    def getUserHome():
+        username = os.getenv("USER")
+        return "/home/ONEPURDUE/{0}".format(username)
--- PACKAGE.py
+++ PACKAGE.py
@@ -2,6 +2,8 @@ import sys
 import os
 from shutil import copyfile
 from COMMON import Common
+import zipfile
+from os import walk
 
 
 class Package:
@@ -24,14 +26,16 @@ class Package:
                 self.mode = "version"
 
     def backupPreviousRelease(self):
-        previousReleaseLocation = Common.baseDir + Common.releaseFile
-        newLocation = Common.baseDir + "previous.release"
-        copyfile(previousReleaseLocation, newLocation)
-        os.remove(previousReleaseLocation)
+        try:
+            previousReleaseLocation = Common.baseDir + Common.releaseFile
+            newLocation = Common.baseDir + "previous.release"
+            copyfile(previousReleaseLocation, newLocation)
+            os.remove(previousReleaseLocation)
+        except:
+            return
 
     def setCurrentRelease(self, releaseNumber):
         versionFile = Common.baseDir + Common.versionFile
-        os.remove(versionFile)
         file = open(versionFile, "w")
         file.write(releaseNumber)
         file.close()
@@ -43,7 +47,47 @@ class Package:
         print "-v\t--v\t\tGet the latest version"
         print "-h\t--h\t\tPrint this help screen"
 
-    def createReleaseFile(self):
+    def getFiles(self, directory):
+        f = []
+        for (dirpath, dirnames, filenames) in walk(directory):
+            cleaned = dirpath.split(directory)[1]
+            if '\\' in cleaned:
+                cleaned = cleaned.split('\\')[0]
+            if '/' in cleaned:
+                cleaned = cleaned.split('/')[0]
+            if not cleaned in Common.ignoreDirectories:
+                # f.extend(dirpath + '/' + filenames)
+                for file in filenames:
+                    if dirpath == directory:
+                        f.append(file)
+                    else:
+                        f.append(dirpath + '/' + file)
+        return f
+
+    def createReleaseFile(self, filename, directory):
+        files = self.getFiles(directory)
+        if len(files) == 0:
+            print "Empty directory, can't create release file"
+            return False
+        else:
+            print "{0} files in directory, creating release file".format(len(files))
+        with zipfile.ZipFile(filename, 'w') as myzip:
+            # myzip.write('eggs.txt')
+            i = 0
+            for file in files:
+                if i % 5 == 0:
+                    print "{0}/{1}".format(i, len(files))
+                myzip.write(file)
+                i = i + 1
+        return True
+
+    def storeReleaseFile(self, filename):
+        self.backupPreviousRelease()
+        releaseFile = Common.baseDir + Common.releaseFile
+        copyfile(filename, releaseFile)
+        # Store in releases folder
+        releasesFolder = Common.baseDir + Common.releasesDir + filename
+        copyfile(filename, releasesFolder)
 
     def main(self):
         print "larry - release helper"
@@ -51,18 +95,34 @@ class Package:
             print "latest version: {0}".format(Common.getLatestVersion())
         elif self.mode == "new":
             directory = raw_input("directory: ")
+            latestVersion = Common.getLatestVersion()
             releaseNumber = None
             while releaseNumber is None:
                 releaseNumber = raw_input("release number: ")
                 try:
-                    number = float(releaseNumber)
+                    float(releaseNumber)    # verify it's an actual number
                 except:
                     print "release number is invalid"
                     releaseNumber = None
-            
+                if self.setVersion and Common.mycmp(releaseNumber, latestVersion) != 1:
+                    print "release number not greater than current version {0}".format(latestVersion)
+                    response = raw_input("do you want to set a different number (y/n)? ")
+                    if response == "y":
+                        releaseNumber = None
+            # create release file
+            filename = "{0}.release".format(releaseNumber)
+            if self.createReleaseFile(filename, directory):
+                print "Created release file at {0}".format(filename)
+            else:
+                print "Failed to create release file, quitting"
+                return
+            self.storeReleaseFile(filename)
+            if self.setVersion:
+                self.setCurrentRelease(releaseNumber)
 
         else:
             self.printHelp()
+
 
 if __name__ == "__main__":
     Package().main()
