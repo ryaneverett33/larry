dade814e4d4a87d05687cb389fe3099f146d5571
Ryan Everett <ryaneverett33@outlook.com>
2019-04-24 11:19:40 -0400

    Merge branch servicesTimeout

--- larry.py
+++ larry.py
@@ -8,12 +8,16 @@ from Hosts import Hosts
 from INSTALL import Install
 from Logger import Logger
 from COMMON import Common
+from ui import ConfigTicket
+from ui import VerifyTicket
 
 
 class larry:
     ui = None
     ignoreUpdate = False
     disablePersistence = False
+    doTicket = None
+    verifyTicket = None
 
     def __init__(self):
         self.ui = UI(self)
@@ -29,10 +33,17 @@ class larry:
             PersistenceModule.getInstance()
         else:
             PersistenceModule.DISABLED = True
+        if self.doTicket is not None:
+            return ConfigTicket.ConfigTicket(None).main(self.doTicket)
+        elif self.verifyTicket is not None:
+            return VerifyTicket.VerifyTicket(None).main(self.verifyTicket)
         self.ui.main()
 
-    def showHelp(self):
+    def __showHelp(self):
         print "usage: larry [option]"
+        print "  -t, --t NUM\t\t\t\tDo a ticket and exit"
+        print "  -v, --v NUM\t\t\t\tVerify a ticket and exit"
+        print ""
         print "  -editor, --editor\t\t\t[DEBUG]Force password utility to act as if it's in an editor"
         print "  -hostsDebug, --hostsDebug\t\t[DEBUG]Force larry to use the debug version of the hosts file"
         print "  -ignoreUpdate, --ignoreUpdate\t\tDisallow larry to check for updates on launch"
@@ -42,9 +53,19 @@ class larry:
         print "  -disableColor, --disableColor\t\tDisable color output"
         print "  -disablePM, --disablePM\t\tDisables the Persistence Module"
         print "  -h, --h\t\t\t\tDisplays this help screen"
+        print "  -V, --V\t\t\t\tDisplays the current version"
+
+    def __invalidTicketFormat(self, arg):
+        print "Invalid Ticket Number"
+        print "USAGE: larry {0} NUMBER, e.g. larry {0} 69420".format(arg)
+        exit(-1)
+
+    def __printVersion(self):
+        print "larry version {0}".format(Common.VERSION)
 
     def parseArgs(self):
-        for arg in sys.argv:
+        for i in range(0, len(sys.argv)):
+            arg = sys.argv[i]
             if arg == "--editor" or arg == "-editor":
                 PasswordUtility.useEditor()
             elif arg == "--hostsDebug" or arg == "-hostsDebug":
@@ -61,9 +82,31 @@ class larry:
                 Logger.disableColor = True
             elif arg == "--disablePM" or arg == "-disablePM":
                 self.disablePersistence = True
+            elif arg == "-t" or arg == "--t":
+                if i + 1 >= len(sys.argv):
+                    self.__invalidTicketFormat(arg)
+                else:
+                    self.doTicket = sys.argv[i + 1]
+                    try:
+                        int(self.doTicket)
+                    except Exception:
+                        self.__invalidTicketFormat(arg)
+                    i = i + 1
+            elif arg == "-v" or arg == "--v":
+                if i + 1 >= len(sys.argv):
+                    self.__invalidTicketFormat(arg)
+                else:
+                    self.verifyTicket = sys.argv[i + 1]
+                    try:
+                        int(self.verifyTicket)
+                    except Exception:
+                        self.__invalidTicketFormat(arg)
+                    i = i + 1
             elif arg == "--h" or arg == "-h":
-                self.showHelp()
+                self.__showHelp()
                 exit(1)
+            elif arg == "--V" or arg == "-V":
+                self.__printVersion()
 
 
 if __name__ == "__main__":
